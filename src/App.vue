149943b03aefa05e3b492723265c1b2fd9b23675
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2021-12-06 18:33:03 +0800

    added rules and user cards

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,138 @@
 <template>
+  
+  <section class="section-wrapper-mini">
+    <div class="squish">
+      <h2>Rules</h2>
+      <ol>
+        <li>Lorem, ipsum dolor sit amet consectetur adipisicing.</li>
+        <li>Minus non modi fugit, rerum quae quos?</li>
+        <li>Obcaecati eius amet blanditiis? Est, id dolorum.</li>
+        <li>Corporis eos labore quam hic molestiae illo!</li>
+        <ol id="sub-section">
+          <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi assumenda cum quo magnam perspiciatis.</li>
+          <li>Ipsa ea dicta, eveniet nostrum, temporibus veritatis sit, quaerat doloribus voluptatibus modi magnam dolorem?</li>
+          <li>Non ex qui iste suscipit, aliquid atque dolorum voluptates dolores et, numquam autem hic!</li>
+          <li>Corporis, animi quis rem dicta obcaecati deleniti reprehenderit minima dignissimos exercitationem quisquam ullam sequi.</li>
+          <li>Amet vel veniam, ad at fugiat officiis illo a earum illum enim impedit dolore!</li>
+          <li>Rem possimus dolorem dolore nesciunt molestias, laborum deserunt soluta, quam nulla doloremque est. Perspiciatis!</li>
+          <li>Ullam reiciendis fuga praesentium veritatis quas, dignissimos ex, eveniet quia, dicta excepturi ea perferendis!</li>
+        </ol>
+        <li>Tempore, quidem ad eveniet esse aliquid non?</li>
+      </ol>
+    </div>
+  </section>
+  <section class="section-wrapper-mini">
+    <div class="squish">
+      <h2>Server staff lol</h2>
+      <div class="card-layout">
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+        <UserCard />
+      </div>
+    </div>
+  </section>
   <footer>
-    <div id="copyright">&copy; {{ new Date().getFullYear()}} | site built by @skepfusky in <img alt="Vue logo" src="./assets/img/vue-logo.png"> Vue.js.</div>
+    <div id="copyright">
+      &copy; {{ new Date().getFullYear() }}, site built by @skepfusky in
+      <img alt="Vue logo" src="./assets/img/vue-logo.png" /> Vue.js.
+    </div>
     <div id="social-row">
       <ul>
-        <li><a target="__blank" href="#">social shit</a></li>
-        <li><a target="__blank" href="#">social shit</a></li>
-        <li><a target="__blank" href="#">social shit</a></li>
-        <li><a target="__blank" href="#">social shit</a></li>
-        <li><a target="__blank" href="#">social shit</a></li>
+        <li><a target="__blank" href="#">youtube</a></li>
+        <li><a target="__blank" href="#">twitter</a></li>
+        <li><a target="__blank" href="#">instagram</a></li>
+        <li><a target="__blank" href="#">github</a></li>
       </ul>
     </div>
   </footer>
 </template>
 
 <script>
+import UserCard from "./components/UserCard.vue";
+
 export default {
-  name: 'wild-times',
-}
+  name: "wild-times",
+  components: {
+    UserCard,
+  },
+};
 </script>
 
 <style lang="scss">
-// Color palette
-
-// Mixins
-@mixin flex-param($hcenter: center, $vcenter: center, $dir: row) {
-  display: flex;
-  display: -webkit-flex;
-  justify-content: $hcenter;
-  align-items: $vcenter;
-  flex-direction: $dir;
-}
+@import "assets/scss/mixins.scss";
 
 // Some boring crap
-*, *::before, *::after {
+body {
+  overflow-x: hidden;
+}
+
+*,
+*::before,
+*::after {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
+
 a {
   text-decoration: none;
   color: inherit;
 }
+
 ul {
   list-style: none;
 }
 
 body {
-  font-family: 'Lato', 'Noto Sans KR', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-family: "Lato", "Noto Sans KR", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   font-size: 110%;
 }
+
 // Da meat
+.squish {
+  width: 1280px;
+  margin: 0 auto;
+}
+
+section {
+  &:nth-child(odd) {
+    background: lighten(green, 25%);
+  }
+  &:nth-child(even) {
+    background: lighten(green, 15%);
+  }
+}
+
+.section-wrapper {
+  &-mini {
+    padding-block: 2rem;
+
+    ol {
+      margin-left: 1.5rem;
+
+      #sub-section {
+        li {
+          list-style-type: lower-alpha;
+        }
+      }
+    }
+
+    li {
+      &::marker {
+        color: darkblue !important;
+      }
+    }
+  }
+}
+.card-layout {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+}
+
 footer {
   @include flex-param(center, center, column);
   width: 100%;
@@ -61,12 +144,15 @@ footer {
     @include flex-param(center, unset, row);
     margin-top: 7px;
   }
+
   li {
     margin-left: 20px;
+
     &:first-child {
       margin-left: 0;
     }
   }
+
   #copyright {
     img {
       position: relative;
@@ -75,5 +161,4 @@ footer {
     }
   }
 }
-
 </style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="cd-wrapper">
+    <div class="cd-info">
+      <img :src="require(`@/assets/img/${avatar}`)">
+      <h2>{{ name }}</h2>
+      <p>{{ role }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+  props: {
+    avatar: {type: String, default: "skep.png"}, // use my stupid profile pic when value is not defined lol
+    name: {type: String, default: "Name"},
+    role: {type: String, default: "Role"}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/mixins.scss';
+
+.cd {
+  &-wrapper {
+    @include flex-param(center, unset, row);
+    margin: 0 auto;
+    width: 19.5rem;
+  }
+
+  &-info {
+    @include flex-param(center, center, column);
+    text-align: center;
+    height: 20rem;
+    box-shadow: 0 0 12px rgba(black, 25%);
+    border-radius: 6px;
+    padding: 1rem;
+    width: 100%;
+
+    img {
+      width: 127px;
+      border-radius: 16px;
+      box-shadow: 0 0 12px rgba(black, 25%);
+    }
+
+    :is(h2, p) {
+      cursor: default;
+      margin-top: 8px;
+    }
+  }
+}
+</style>
