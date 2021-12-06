dcc55e0bddc3fa7bec0d0484c3bc5f1e07ad2042
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2021-12-06 10:51:16 +0800

    init commit

--- README.md
+++ README.md
@@ -1,19 +1,10 @@
-# wild-times-vue
+#  Wild Times website
 
-## Project setup
-```
-npm install
-```
+A website for a chill and SFW furry server, **Wild Times!** But you may ask, why would you make a website for a Discord server? Well, firstly, to learn more on building apps (or terrible apps/website) using a JavaScript framework, Vue.js, since having an understandable knowledge if HTML, CSS, and barely any JavaScript at all lol
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+And secondly, it may seem silly to make a website from a Discord server but I've met and talked to a lot of cool and awesome people there so far!
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Some stuff used
+Unless anyone's a developer or as *really* tech-savvy as I am, no one would care but if you're curious - yes, this site is built using the latest version of Vue, version 3 through Vue CLI and some other dependencies such as FontAwesome and Google Fonts.
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+It also uses a CSS pre-processor, Sass or SCSS, to provide more flexibility and efficiency when it comes to web design. More specifically, I used "dart-sass" because **y e s**.
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,79 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <footer>
+    <div id="copyright">&copy; {{ new Date().getFullYear()}} | site built by @skepfusky in <img alt="Vue logo" src="./assets/img/vue-logo.png"> Vue.js.</div>
+    <div id="social-row">
+      <ul>
+        <li><a target="__blank" href="#">social shit</a></li>
+        <li><a target="__blank" href="#">social shit</a></li>
+        <li><a target="__blank" href="#">social shit</a></li>
+        <li><a target="__blank" href="#">social shit</a></li>
+        <li><a target="__blank" href="#">social shit</a></li>
+      </ul>
+    </div>
+  </footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'wild-times',
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+// Color palette
+
+// Mixins
+@mixin flex-param($hcenter: center, $vcenter: center, $dir: row) {
+  display: flex;
+  display: -webkit-flex;
+  justify-content: $hcenter;
+  align-items: $vcenter;
+  flex-direction: $dir;
 }
+
+// Some boring crap
+*, *::before, *::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: 'Lato', 'Noto Sans KR', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 110%;
+}
+// Da meat
+footer {
+  @include flex-param(center, center, column);
+  width: 100%;
+  background: darken(orchid, 25%);
+  color: white;
+  padding: 1rem 0;
+
+  ul {
+    @include flex-param(center, unset, row);
+    margin-top: 7px;
+  }
+  li {
+    margin-left: 20px;
+    &:first-child {
+      margin-left: 0;
+    }
+  }
+  #copyright {
+    img {
+      position: relative;
+      top: 2px;
+      width: 17px;
+    }
+  }
+}
+
 </style>
